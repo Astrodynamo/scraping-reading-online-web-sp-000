1221ac02f7b4c66f3afb654e75415c6edf0523a3
Amanda King <amapickell@gmail.com>
2019-08-04 17:52:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,7 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("http://flatironschool.com/"))
-puts doc.css(".site-header__hero__headline").text.strip
+line = doc.css(".site-header__hero__headline").text.strip
 
-
+puts line
 
